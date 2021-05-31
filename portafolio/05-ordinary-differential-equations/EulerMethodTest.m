dd450316fe685d921832fff7596019198039b6d4
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2021-05-31 14:42:57 -0600

    [Update] - Euler Method Test

--- portafolio/05-ordinary-differential-equations/EulerMethodTest.m
+++ portafolio/05-ordinary-differential-equations/EulerMethodTest.m
@@ -1,9 +1,28 @@
-func = @(t, q) 1/3 - q / 6
-a = 0
-b = 30
-q0 = 0
-n = 3
+func = @(t, y) y .* e.^(2 .* t)
 
-[yApprox] = EulerMethod(func, q0, a, b, n)
+a = 1
+b = 2
+y0 = 1
+n = 5
 
-plot(a:(b - a) / n:b, yApprox)
+[yApprox] = EulerMethod(func, y0, a, b, n)
+
+x = linspace(a, b, n + 1);
+sol = @(x) 1 / 25 .* e.^(-2 .* x) .* (e.^(5 .* x) .* (5 .* x - 1) + 1)
+
+figure(1)
+
+plot(x, yApprox);
+legend('Approx');
+
+hold on
+
+plot(x, sol(x));
+legend('Solution');
+hold off
+
+figure(2)
+
+plot(x, sol(x) - yApprox);
+title('Error');
+hold off
